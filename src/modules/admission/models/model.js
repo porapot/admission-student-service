1c5f3e785e19246825e7b0e6a9b90c7f1e1e602b
porapot <porapot2@gmail.com>
2019-07-24 12:51:20 +0700

    first commit

---
+++ src/modules/admission/models/model.js
@@ -0,0 +1,79 @@
+'use strict';
+// use model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var AdmissionSchema = new Schema({
+
+    school_year: {
+        type: String
+    },
+    student_no: {
+        type: String
+    },
+    identification_number: {
+        type: String
+    },
+    fullname_th: {
+        type: String,
+        required: 'Please fill a Admission fullname_th'
+    },
+    nickname_th: {
+        type: String
+    },
+    fullname_eng: {
+        type: String
+    },
+    nickname_eng: {
+        type: String
+    },
+    dateofbirth: {
+        type: String
+    },
+    student_id: {
+        type: String,
+        required: 'Please fill a Admission student_id'
+    },
+    firstdayofschool: {
+        type: String
+    },
+    classroom:{
+        type: String
+    },
+    telno:{
+        type: String
+    },
+
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date
+    },
+    createby: {
+        _id: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        displayname: {
+            type: String
+        }
+    },
+    updateby: {
+        _id: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        displayname: {
+            type: String
+        }
+    }
+});
+
+mongoose.model("Admission", AdmissionSchema);
